5952612ba647d298aa433e32d67ec0d9d8b132d9
Stals <superstals@gmail.com>
2014-09-08 23:53:36 +0400

    jave convert

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -6,13 +6,76 @@
 #include <cmath>
 #include <cstdlib>
 
-using namespace model;
+
 using namespace std;
 
-void MyStrategy::move(const Hockeyist& self, const World& world, const Game& game, Move& move) {
-    move.setSpeedUp(-1.0);
-    move.setTurn(PI);
-    move.setAction(STRIKE);
-}
+const double STRIKE_ANGLE = 1.0 * PI / 180.0;
+
 
 MyStrategy::MyStrategy() { }
+
+
+void MyStrategy::move(const Hockeyist& self, const World& world, const Game& game, Move& move) {
+	if (self.getState() == SWINGING) {
+        move.setAction(STRIKE);
+        return;
+    }
+
+    if (world.getPuck().getOwnerPlayerId() == self.getPlayerId()) {
+        if (world.getPuck().getOwnerHockeyistId() == self.getId()) {
+            Player opponentPlayer = world.getOpponentPlayer();
+
+            double netX = 0.5 * (opponentPlayer.getNetBack() + opponentPlayer.getNetFront());
+            double netY = 0.5 * (opponentPlayer.getNetBottom() + opponentPlayer.getNetTop());
+            netY += (self.getY() < netY ? 0.5 : -0.5) * game.getGoalNetHeight();
+
+            double angleToNet = self.getAngleTo(netX, netY);
+            move.setTurn(angleToNet);
+
+            if (abs(angleToNet) < STRIKE_ANGLE) {
+                move.setAction(SWING);
+            }
+        } else {
+            const Hockeyist* nearestOpponent = getNearestOpponent(self.getX(), self.getY(), world);
+            if (nearestOpponent != 0) {
+                if (self.getDistanceTo(*nearestOpponent) > game.getStickLength()) {
+                    move.setSpeedUp(1.0);
+                } else if (abs(self.getAngleTo(*nearestOpponent)) < 0.5 * game.getStickSector()) {
+                    move.setAction(STRIKE);
+                }
+                move.setTurn(self.getAngleTo(*nearestOpponent));
+            }
+        }
+    } else {
+        move.setSpeedUp(1.0);
+        move.setTurn(self.getAngleTo(world.getPuck()));
+        move.setAction(TAKE_PUCK);
+    }
+}
+
+ const Hockeyist* MyStrategy::getNearestOpponent(double x, double y, const World &world) {
+        const Hockeyist* nearestOpponent = 0;
+        double nearestOpponentRange = 0.0;
+
+		const std::vector<model::Hockeyist>& hockeists = world.getHockeyists();
+		size_t size = hockeists.size();
+
+		for(size_t i = 0; i < size; ++i){
+			const Hockeyist& hockeyist = hockeists[i];
+       
+            if (hockeyist.isTeammate() || hockeyist.getType() == GOALIE
+                    || hockeyist.getState() == KNOCKED_DOWN
+                    || hockeyist.getState() == RESTING) {
+                continue;
+            }
+
+            double opponentRange = hypot(x - hockeyist.getX(), y - hockeyist.getY());
+
+            if (nearestOpponent == 0 || opponentRange < nearestOpponentRange) {
+                nearestOpponent = &hockeyist;
+                nearestOpponentRange = opponentRange;
+            }
+        }
+
+        return nearestOpponent;
+    }
--- MyStrategy.h
+++ MyStrategy.h
@@ -5,11 +5,16 @@
 
 #include "Strategy.h"
 
+using namespace model;
+
 class MyStrategy : public Strategy {
 public:
     MyStrategy();
 
     void move(const model::Hockeyist& self, const model::World& world, const model::Game& game, model::Move& move);
+
+private:
+	static const model::Hockeyist* getNearestOpponent(double x, double y, const World &world);
 };
 
 #endif
